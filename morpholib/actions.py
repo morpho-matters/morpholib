3e171c32b33ebd36efa5655b0764ab339d63594c
morpho.matters <morpho.matters@gmail.com>
2022-03-24 23:26:36 -0500

    Added documentation
    Added comments explaining how fadeOut(), fadeIn(), and rollback() work.

--- morpholib/actions.py
+++ morpholib/actions.py
@@ -9,8 +9,27 @@ import morpholib as morpho
 # from morpho.tools.basics import *
 # import math, cmath
 
-# TODO: Add "stagger" argument to both fadeOut() and rollback()
-
+# Convenience function fades out a collection of actors and then
+# sets the visibility attribute of the final keyfigure of each actor
+# set to False.
+#
+# NOTE: Only works for figures that possess an "alpha" tweenable, so
+# this function may not work for custom figures like Skits unless you
+# implement an alpha tweenable yourself.
+#
+# PARAMETERS
+# actors = List of actors (or a single actor)
+# duration = Duration (in frames) of fade out for each actor.
+#            Default: 30
+# atFrame = Frame index at which to begin fade out.
+#           Default: the latest keyindex among all the given actors.
+# stagger = Number of frames to wait between starting fade outs of
+#           adjacent actors in the list. Leads to a "staggering" effect.
+#           Example: stagger=5 means the second actor will start fading
+#           5 frames after the first actor STARTS fading. Likewise,
+#           the third actor will begin fading 5 frames after the
+#           second actor starts fading, and so on.
+#           Default: 0 (meaning all actors begin fading at the same time)
 def fadeOut(actors, duration=30, atFrame=None, stagger=0):
     # if not isinstance(actors, list) and not isinstance(actors, tuple):
     #     actors = [actors]
@@ -25,6 +44,14 @@ def fadeOut(actors, duration=30, atFrame=None, stagger=0):
         actor.newendkey(duration).alpha = 0
         actor.last().visible = False
 
+# Similar to fadeOut(), but fades in actors from (presumed) invisibility.
+# See fadeOut() for more info.
+#
+# NOTE: Only works for figures that possess an "alpha" tweenable, so
+# this function may not work for custom figures like Skits unless you
+# implement an alpha tweenable yourself.
+#
+# Only intended to work for actors whose final keyfigure has alpha = 0.
 def fadeIn(actors, duration=30, atFrame=None, stagger=0):
     # if not isinstance(actors, list) and not isinstance(actors, tuple):
     #     actors = [actors]
@@ -38,6 +65,27 @@ def fadeIn(actors, duration=30, atFrame=None, stagger=0):
         actor.newkey(atFrame+n*stagger).visible = True
         actor.newendkey(duration).alpha = 1
 
+# Convenience function tweens an actor back to its first keyfigure
+# and then sets the visibility attribute of the final keyfigure of
+# each actor set to False.
+#
+# Intended to be used when the the first keyfigure of an actor
+# is invisible or offscreen. It's a nice way to make an actor
+# gracefully leave a scene the same way it entered.
+#
+# PARAMETERS
+# actors = List of actors (or a single actor)
+# duration = Duration (in frames) of rollback animation for each actor.
+#            Default: 30
+# atFrame = Frame index at which to begin rollback animation.
+#           Default: the latest keyindex among all the given actors.
+# stagger = Number of frames to wait between starting rollbacks of
+#           adjacent actors in the list. Leads to a "staggering" effect.
+#           Example: stagger=5 means the second actor will start rolling
+#           back 5 frames after the first actor STARTS fading. Likewise,
+#           the third actor will begin rolling back 5 frames after the
+#           second actor starts rolling back, and so on.
+#           Default: 0 (all actors begin rolling back at the same time)
 def rollback(actors, duration=30, atFrame=None, stagger=0):
     # if not isinstance(actors, list) and not isinstance(actors, tuple):
     #     actors = [actors]
