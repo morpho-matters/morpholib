2944cd02bfbd8274307b4ce94fffb76ac50e0f65
morpho.matters <morpho.matters@gmail.com>
2022-04-30 10:22:10 -0500

    Moved "color" module out of tools subdirectory
    This makes the color module accessible directly via import morpholib.color
    
    For backward compatibility, it can still be accessed from tools using the old style: import morpholib.tools.color

--- morpholib/__init__.py
+++ morpholib/__init__.py
@@ -1,7 +1,7 @@
 from morpholib.base import *
 from morpholib.figure import *
 from morpholib.anim import Layer, Animation, Frame, SpaceFrame, Skit, SpaceSkit, SkitParameters, SpaceLayer, MultiFigure, SpaceMultiFigure
-import morpholib.tools.color as color
+import morpholib.color as color
 from morpholib.matrix import array
 
 from morpholib.tools.subimporter import import_submodules
