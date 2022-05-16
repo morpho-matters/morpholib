de6cb0820b92637ae2fa4dbe0b7962a85d6f9609
morpho.matters <morpho.matters@gmail.com>
2022-05-16 16:01:59 -0500

    Added `jump` optional argument to fadeOut()

--- morpholib/actions.py
+++ morpholib/actions.py
@@ -30,19 +30,37 @@ import morpholib as morpho
 #           the third actor will begin fading 5 frames after the
 #           second actor starts fading, and so on.
 #           Default: 0 (meaning all actors begin fading at the same time)
-def fadeOut(actors, duration=30, atFrame=None, stagger=0):
+def fadeOut(actors, duration=30, atFrame=None, stagger=0, jump=()):
     # if not isinstance(actors, list) and not isinstance(actors, tuple):
     #     actors = [actors]
     # Turn into a list if necessary
     if isinstance(actors, morpho.Actor):
         actors = [actors]
+
     if atFrame is None:
         atFrame = max(actor.lastID() for actor in actors)
+
+    # If jump isn't a subscriptable type, turn it into a singleton list
+    if not hasattr(jump, "__getitem__"):
+        jump = [jump]
+
     for n in range(len(actors)):
         actor = actors[n]
         actor.newkey(atFrame+n*stagger)
-        actor.newendkey(duration).alpha = 0
-        actor.last().visible = False
+        keyfig = actor.newendkey(duration)
+        keyfig.alpha = 0
+        keyfig.visible = False
+        if len(jump) > 0:
+            dz = jump[n%len(jump)]
+
+            # Add dz in place to either the "pos" or "origin"
+            # attributes (if they exist).
+            if "pos" in keyfig._state:
+                keyfig.pos += dz
+            elif "origin" in keyfig._state:
+                keyfig.origin += dz
+            else:
+                raise TypeError(f"{type(keyfig)} figure cannot be jumped.")
 
 # Similar to fadeOut(), but fades in actors from (presumed) invisibility.
 # See fadeOut() for more info.
@@ -52,7 +70,7 @@ def fadeOut(actors, duration=30, atFrame=None, stagger=0):
 # implement an alpha tweenable yourself.
 #
 # Only intended to work for actors whose final keyfigure has alpha = 0.
-def fadeIn(actors, duration=30, atFrame=None, stagger=0):
+def fadeIn(actors, duration=30, atFrame=None, stagger=0, jump=0):
     # if not isinstance(actors, list) and not isinstance(actors, tuple):
     #     actors = [actors]
     # Turn into a list if necessary
