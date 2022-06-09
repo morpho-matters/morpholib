a0cbbf2678145e4b21602d51858e9fe659f96999
morpho.matters <morpho.matters@gmail.com>
2022-06-09 13:24:17 -0500

    Imported `action` from `morpho.actions` into base namespace
    This means `morpho.actions.action.myaction(...)` can now be accessed with the simpler syntax
    `morpho.action.myaction(...)`

--- morpholib/__init__.py
+++ morpholib/__init__.py
@@ -3,6 +3,7 @@ from morpholib.figure import *
 from morpholib.anim import Layer, Animation, Frame, SpaceFrame, Skit, SpaceSkit, SkitParameters, SpaceLayer, MultiFigure, SpaceMultiFigure
 import morpholib.color as color
 from morpholib.matrix import array
+from morpholib.actions import action
 
 from morpholib.tools.subimporter import import_submodules
 
