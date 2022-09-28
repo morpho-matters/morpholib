b76a503261ecfb688974a9cfeb15b0e9f2cd6387
morpho.matters <morpho.matters@gmail.com>
2022-09-27 23:10:05 -0500

    Implemented first draft of `invertBezier2d()`

--- morpholib/bezier.py
+++ morpholib/bezier.py
@@ -3,7 +3,7 @@ Various helper functions for dealing with cubic Bezier curves.
 '''
 
 # import math, cmath
-# import numpy as np
+import numpy as np
 from morpholib.base import lerp0, bezierInterp
 
 # Splits the cubic Bezier curve described by p0...p3 at the
@@ -34,3 +34,38 @@ def splitBezier(p0, p1, p2, p3, t):
     slice1 = bezierFirstSlice(p0, p1, p2, p3, t)
     slice2 = bezierLastSlice(p0, p1, p2, p3, t)
     return slice1, slice2
+
+# Inverts the bezier function to find the parameter t value
+# for a given 2d point z (as a complex number) that is
+# assumed to be on the bezier curve.
+#
+# Returns all solutions found as a list.
+# Usually the list will just contain a single solution,
+# but it could contain two (self-intersecting curve)
+# or none (given point not on the curve).
+def invertBezier2d(p0, p1, p2, p3, z):
+    # Calculate coefficients of the polynomial equation
+    # bezier(t) - z = 0
+    coeffs = np.array([
+        p3 - 3*p2 + 3*p1 - p0,
+        3*(p2 - 2*p1 + p0),
+        3*(p1 - p0),
+        p0 - z
+        ])
+
+    # Solve for the roots
+    txlist = np.roots(coeffs.real).tolist()
+    tylist = np.roots(coeffs.imag).tolist()
+
+    # Filter out any common real roots
+    tol = 1e-9
+    solns = []
+    for tx in txlist:
+        # Skip the tx solutions that are obviously non-real
+        if abs(tx.imag) > tol:
+            continue
+        for ty in tylist:
+            if abs(tx - ty) < tol:
+                solns.append(tx)
+
+    return solns
