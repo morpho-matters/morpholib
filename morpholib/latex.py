2b8589b251a7292ffc5fb5bb7177811e76e08b80
morpho.matters <morpho.matters@gmail.com>
2023-02-04 16:22:51 -0600

    Added comments to `latex` module.

--- morpholib/latex.py
+++ morpholib/latex.py
@@ -1,3 +1,8 @@
+'''
+Contains code to facilitate parsing and rendering LaTeX.
+Note that to use the functions in this module, you will
+need to have LaTeX installed on your system.
+'''
 
 import morpholib as morpho
 import morpholib.tools.latex2svg as latex2svg
@@ -10,14 +15,27 @@ params = latex2svg.default_params.copy()
 _preamble = preamble
 _params = params
 
-def sanitizeTex(tex):
+# Mainly for internal use.
+# Takes LaTeX code and surrounds it with \( \) if it
+# doesn't already. These are needed for the LaTeX
+# parser to work.
+def _sanitizeTex(tex):
     tex = tex.strip()
     if r"\(" not in tex:
         tex = r"\(" + tex + r"\)"
     return tex
 
+# Parses a string containing LaTeX code and returns a
+# MultiSpline figure representing it.
+# Optionally a `preamble` keyword argument can be specified.
+# This is mainly to change which packages are imported when
+# the LaTeX is parsed. If unspecified, the preamble will be
+# taken from morpho.latex.preamble.
+#
+# Any other args/kwargs will be passed into the MultiSpline
+# constructor (e.g. boxWidth)
 def tosvg(tex, *args, preamble=None, **kwargs):
-    tex = sanitizeTex(tex)
+    tex = _sanitizeTex(tex)
     if preamble is None:
         preamble = _preamble
     params = _params.copy()
