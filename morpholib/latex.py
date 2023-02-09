a1303c42841061a3308f529454fe39784864e653
morpho.matters <morpho.matters@gmail.com>
2023-02-08 21:25:31 -0600

    Enabled TeX SVG caching.
    Massively speeds up generating LaTeX splines if they've been generated in the past.

--- morpholib/latex.py
+++ morpholib/latex.py
@@ -4,7 +4,7 @@ Note that to use the functions in this module, you will
 need to have LaTeX installed on your system.
 '''
 
-import io
+import os, io, hashlib
 
 import morpholib as morpho
 import morpholib.tools.latex2svg as latex2svg
@@ -17,6 +17,28 @@ template = latex2svg.default_template
 preamble = latex2svg.default_preamble
 params = latex2svg.default_params.copy()
 
+# Directory in which to cache LaTeX code converted to SVG
+# By default, it's None, meaning caching is disabled.
+cacheDir = None
+
+# Number of hex digits to use as part of the hash
+cacheHashLength = 32
+
+# Takes a string as input and returns a string
+# which is the input string's SHA-256 hash expressed
+# in hexadecimal notation.
+# To convert to a standard integer, run the following:
+#
+# int(sha256(strng), 16)
+#
+# This function currently only accepts UTF-8 strings
+# for input. Someday (probably when need arises) I'll
+# generalize it for other kinds of input.
+def sha256(strng):
+    sha = hashlib.sha256()
+    sha.update(bytes(strng, "utf-8"))
+    return sha.hexdigest()
+
 # Mainly for internal use.
 # Takes LaTeX code and surrounds it with \( \) if it
 # doesn't already. These are needed for the LaTeX
@@ -26,6 +48,18 @@ def _sanitizeTex(tex):
     if r"\(" not in tex:
         tex = r"\(" + tex + r"\)"
     return tex
+
+# Returns a filename for the given TeX code that can be
+# used to cache the SVG the TeX code was converted into.
+def hashTex(tex):
+    texhash = sha256(tex)[:cacheHashLength]
+    return f"tex-{texhash}.svg"
+
+# Returns boolean on whether the given filename is
+# in the current cache directory.
+def iscached(tex):
+    filename = hashTex(tex)
+    return filename in os.listdir(cacheDir)
 
 # Parses a string containing LaTeX code and returns a
 # MultiSpline figure representing it.
@@ -38,6 +72,12 @@ def _sanitizeTex(tex):
 # constructor (e.g. boxWidth)
 def parse(tex, *args, preamble=None, **kwargs):
     tex = _sanitizeTex(tex)
+
+    # Check if the SVG for this TeX code is cached
+    if cacheDir is not None and iscached(tex):
+        filepath = cacheDir + os.sep + hashTex(tex)
+        return morpho.shapes.MultiSpline.fromsvg(filepath, *args, **kwargs)
+
     if preamble is None:
         # Referencing the global scope `preamble` variable via
         # the module itself is required here since the local
@@ -47,8 +87,17 @@ def parse(tex, *args, preamble=None, **kwargs):
     params["preamble"] = preamble
 
     out = latex2svg.latex2svg(tex, params)
+    svgcode = out["svg"]
+
+    # If caching is enabled, save the output svg code
+    # as a file in the specified cache directory.
+    if cacheDir is not None:
+        filepath = cacheDir + os.sep + hashTex(tex)
+        with open(filepath, "w") as file:
+            file.write(svgcode)
+
     with io.StringIO() as stream:
-        stream.write(out["svg"])
+        stream.write(svgcode)
         stream.seek(0)
         spline = morpho.shapes.MultiSpline.fromsvg(stream, *args, **kwargs)
     return spline
