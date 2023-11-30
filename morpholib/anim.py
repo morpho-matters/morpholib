eeb8a0a6173a8482ba777d15215f4834d2cc31ee
morpho.matters <morpho.matters@gmail.com>
2023-11-30 13:37:51 -0600

    Clarified comment

--- morpholib/anim.py
+++ morpholib/anim.py
@@ -611,8 +611,9 @@ class Frame(BoundingBoxFigure):
 
     # Given a Frame of subframes generated from calling partition(),
     # combine() recombines them back into a single Frame figure
-    # and returns the result. Note that this method leaves the
-    # original Frame figure that called it unchanged.
+    # and returns the result. Note that this method creates copies
+    # of all subfigures and leaves the original Frame figure that
+    # called it unchanged.
     #
     # This method may not combine them perfectly if transformation
     # tweenables of the underlying subframes were modified
